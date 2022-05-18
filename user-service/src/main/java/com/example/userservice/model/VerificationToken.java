f71a3c3fb1677d53548d84e1b6798f0aaa86c44a
Dejan Bjelic <dejan.bjelic@uns.ac.rs>
2022-05-18 03:06:34 +0200

    refactor: Changed token id type in repository

--- user-service/src/main/java/com/example/userservice/model/VerificationToken.java
+++ user-service/src/main/java/com/example/userservice/model/VerificationToken.java
@@ -1,8 +1,5 @@
 package com.example.userservice.model;
 
-import com.example.userservice.model.User;
-import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonSetter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
@@ -12,7 +9,7 @@ import java.util.Random;
 @Document
 public class VerificationToken {
     @Id
-    public Long id;
+    public String id;
     public String token;
     public LocalDateTime createdAt;
     public LocalDateTime expiresAt;
@@ -23,7 +20,8 @@ public class VerificationToken {
     public VerificationToken(LocalDateTime createdAt,
                              LocalDateTime expiresAt,
                              User user) {
-        this.id = new Random().nextLong();
+        String id = java.util.UUID.randomUUID().toString();
+        this.id = "token_" + id;
         this.token = java.util.UUID.randomUUID().toString();
         this.createdAt = createdAt;
         this.expiresAt = expiresAt;
--- user-service/src/main/java/com/example/userservice/repository/TokenRepository.java
+++ user-service/src/main/java/com/example/userservice/repository/TokenRepository.java
@@ -4,5 +4,5 @@ import com.example.userservice.model.User;
 import com.example.userservice.model.VerificationToken;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TokenRepository extends MongoRepository<VerificationToken, String> {
+public interface TokenRepository extends MongoRepository<VerificationToken, Long> {
 }
