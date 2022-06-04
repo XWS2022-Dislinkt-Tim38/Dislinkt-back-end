1835cb662242d9a670b5da1fbd07b751c5220f22
Dejan Bjelic <dejan.bjelic@uns.ac.rs>
2022-06-04 16:05:29 +0200

    fix: Added CrossOrigin annotation to post controller

--- post-service/src/main/java/com/example/postservice/controller/PostController.java
+++ post-service/src/main/java/com/example/postservice/controller/PostController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Objects;
 
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping(value = "/post")
 public class PostController {
